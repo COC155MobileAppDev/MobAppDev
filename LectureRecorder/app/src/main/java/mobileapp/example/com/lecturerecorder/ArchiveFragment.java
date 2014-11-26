ec52d011e0a0968bb6084d9180e4a6f1e1d0508c
nandoAC <f.alvan-corcuera-09@student.lboro.ac.uk>
2014-11-26 15:30:21 +0000

    Lecture Recorder App
    Slide menu and Fragments

---
+++ LectureRecorder/app/src/main/java/mobileapp/example/com/lecturerecorder/ArchiveFragment.java
@@ -0,0 +1,26 @@
+package mobileapp.example.com.lecturerecorder;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by fernandoalvan on 26/11/14.
+ */
+
+
+
+public class ArchiveFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        return rootView;
+    }
+
+}
+
+
