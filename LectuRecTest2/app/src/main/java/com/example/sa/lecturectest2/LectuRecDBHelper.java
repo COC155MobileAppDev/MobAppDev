c595a4a19be7c178d60fde3a0cb5d5cab3fb6ddc
Prash93 <Prashanna@hotmail.co.uk>
2014-11-29 20:52:34 +0000

    Using Content provider and DBhelper
    problem: cant retrieve the data in mainactivity from provider and
    dbhelper classes

---
+++ LectuRecTest2/app/src/main/java/com/example/sa/lecturectest2/LectuRecDBHelper.java
@@ -0,0 +1,44 @@
+package com.example.sa.lecturectest2;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Sa on 29/11/2014.
+ */
+public class LectuRecDBHelper extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "LecturerRecorder";
+
+    public static final String TABLE_MODULES = "modules";
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_MODULE = "module";
+
+    private static final String DATABASE_CREATE = "create table "
+            + TABLE_MODULES
+            + "("
+            + COLUMN_ID + " integer primary key autoincrement, "
+            + COLUMN_MODULE + " text not null);";
+
+
+    public LectuRecDBHelper (Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        //creates modules table
+        db.execSQL(DATABASE_CREATE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_MODULES);
+
+        //creates modules table
+        onCreate(db);
+    }
+
+}
---
+++ LectuRecTest2/app/src/main/java/com/example/sa/lecturectest2/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.sa.lecturectest2;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.net.Uri;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView modulesView = (TextView) findViewById(R.id.modulesview);
+
+        Cursor cursor = getModules();
+
+        while (cursor.moveToNext()) {
+            String displayModule = cursor.getString(cursor.getColumnIndex(dbHelper.COLUMN_MODULE));
+            modulesView.append(displayModule);
+            modulesView.append("\n");
+        }
+    }
+
+    LectuRecDBHelper dbHelper;
+
+    private Cursor getModules() {
+        // Run query
+        Uri uri = LectuRecProvider.MODULE_URI;
+        String[] projection = new String[] { dbHelper.COLUMN_ID, dbHelper.COLUMN_MODULE };
+        String selection = null;
+        String[] selectionArgs = null;
+        String sortOrder = null;
+        
+        return managedQuery(uri, projection, selection, selectionArgs, sortOrder);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
