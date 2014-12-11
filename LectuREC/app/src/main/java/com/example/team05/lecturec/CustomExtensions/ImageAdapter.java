9ce3494ba44b90f15f38b6af7ef935b1b9b597e7
nandoAC <f.alvan-corcuera-09@student.lboro.ac.uk>
2014-12-11 07:05:42 +0000

    SingleViewImage ScaleGesture Completed

--- LectuREC/app/src/main/java/com/example/team05/lecturec/CustomExtensions/ImageAdapter.java
+++ LectuREC/app/src/main/java/com/example/team05/lecturec/CustomExtensions/ImageAdapter.java
@@ -19,7 +19,7 @@ import java.util.List;
  */
 public class ImageAdapter extends BaseAdapter {
 
-    private List<Item> images = new ArrayList<Item>();
+    public List<Item> images = new ArrayList<Item>();
     private LayoutInflater inflater;
 
     public ImageAdapter(Context context)
@@ -94,56 +94,3 @@ public class ImageAdapter extends BaseAdapter {
     }
 }
 
-
-     /*
-    private Context mContext;
-
-    // Constructor
-    public ImageAdapter(Context c) {
-        mContext = c;
-    }
-
-    public int getCount() {
-        return mThumbIds.length;
-    }
-
-    public Object getItem(int position) {
-        return null;
-    }
-
-    public long getItemId(int position) {
-        return 0;
-    }
-
-    // create a new ImageView for each item referenced by the Adapter
-    public View getView(int position, View convertView, ViewGroup parent) {
-        ImageView imageView;
-        if (convertView == null) {
-            imageView = new ImageView(mContext);
-            imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
-            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-            imageView.setPadding(8, 8, 8, 8);
-        } else {
-            imageView = (ImageView) convertView;
-        }
-
-        imageView.setImageResource(mThumbIds[position]);
-        return imageView;
-    }
-
-    // Keep all Images in array
-
-    public Integer[] mThumbIds = {
-            R.drawable.sample_2, R.drawable.sample_3,
-            R.drawable.sample_4, R.drawable.sample_5,
-            R.drawable.sample_6, R.drawable.sample_7,
-            R.drawable.sample_0, R.drawable.sample_1,
-            R.drawable.sample_2, R.drawable.sample_3,
-            R.drawable.sample_4, R.drawable.sample_5,
-            R.drawable.sample_6, R.drawable.sample_7,
-            R.drawable.sample_0, R.drawable.sample_1,
-            R.drawable.sample_2, R.drawable.sample_3,
-            R.drawable.sample_4, R.drawable.sample_5,
-            R.drawable.sample_6, R.drawable.sample_7
-    };
-    */
--- LectuREC/app/src/main/java/com/example/team05/lecturec/ViewControllers/ImagesFragment.java
+++ LectuREC/app/src/main/java/com/example/team05/lecturec/ViewControllers/ImagesFragment.java
@@ -1,12 +1,14 @@
 package com.example.team05.lecturec.ViewControllers;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.FrameLayout;
 import android.widget.GridView;
 
@@ -56,8 +58,18 @@ public class ImagesFragment extends Fragment {
         GridView gridview = (GridView) fragmentLayout.findViewById(R.id.gridview);
         gridview.setAdapter(new ImageAdapter(getActivity().getApplicationContext()));
 
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
 
-       return fragmentLayout;
+                // Send intent to SingleViewActivity
+                Intent i = new Intent((getActivity().getApplicationContext()), SingleViewActivity.class);
+                // Pass image index
+                i.putExtra("id", position);
+                startActivity(i);
+            }
+        });
+
+        return fragmentLayout;
 
 
 
